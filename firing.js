5a96c2feed2a980e8a376dcc072f786fb1972434
Alfred Odey <alfredmodey@gmail.com>
2025-02-14 20:51:34 +0100

    Modified firing.js

--- firing.js
+++ firing.js
@@ -19,14 +19,14 @@ function shooting() {
   firingRateDisplay.style.color = "orange";
   firingRateDisplay.innerHTML = "Firing rate: " + firingRate + " Warning of overheating";
   clearInterval(interval);
-  interval = setInterval(decrement, 55);
+  interval = setInterval(decrement, 65);
  } else if (firingRate > 21) {
   firingRate++;
   firingRateDisplay.style.color = "red";
   firing.innerHTML = "Firing: Gun overheating!";
   firingRateDisplay.innerHTML = "Firing rate: Gun overheating!";
   clearInterval(interval);
-  interval = setInterval(decrement, 60);
+  interval = setInterval(decrement, 70);
  }
 }
 
